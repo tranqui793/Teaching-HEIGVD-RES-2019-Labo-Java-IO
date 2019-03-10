36025f5f01501c2636a368dd7ae63ac142d0e3e9
olliens <oussama.lagha@heig-vd.ch>
2019-03-11 00:10:28 +0100

    UtilsTest done

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,10 +7,9 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -26,6 +25,7 @@ public class Application implements IApplication {
    * to where the Java application is invoked.
    */
   public static String WORKSPACE_DIRECTORY = "./workspace/quotes";
+  public static String SEPARATOR =File.separator;
   
   private static final Logger LOG = Logger.getLogger(Application.class.getName());
   
@@ -84,12 +84,10 @@ public class Application implements IApplication {
     QuoteClient client = new QuoteClient();
     for (int i = 0; i < numberOfQuotes; i++) {
       Quote quote = client.fetchQuote();
-      /* There is a missing piece here!
-       * As you can see, this method handles the first part of the lab. It uses the web service
-       * client to fetch quotes. We have removed a single line from this method. It is a call to
-       * one method provided by this class, which is responsible for storing the content of the
-       * quote in a text file (and for generating the directories based on the tags).
-       */
+
+        //storing in a text file the content of the quote
+      storeQuote(quote, new StringBuilder("quote-" + i + ".utf8").toString());
+
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +121,33 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      String directory = new StringBuilder(WORKSPACE_DIRECTORY + SEPARATOR).toString();
+      File workspace = new File(directory);
+      List<String> tags = quote.getTags();
+
+      // repos workspace delete at the beginngof main so we create a new one
+      workspace.mkdirs();
+
+      if (!tags.isEmpty())
+      {
+          // creating the other folders path
+
+          for (String tag : tags) {
+              directory += tag + SEPARATOR;
+          }
+
+          new File(directory).mkdirs();
+      }
+
+      // creating the storage file
+      File file = new File(directory + filename);
+      file.createNewFile();
+
+      // store the quote text in the file
+      Writer writer = new OutputStreamWriter(new FileOutputStream(directory + filename), "UTF-8");
+      writer.write(quote.getQuote());
+      writer.flush();
+      writer.close();
   }
   
   /**
@@ -135,11 +159,13 @@ public class Application implements IApplication {
     explorer.explore(new File(WORKSPACE_DIRECTORY), new IFileVisitor() {
       @Override
       public void visit(File file) {
-        /*
-         * There is a missing piece here. Notice how we use an anonymous class here. We provide the implementation
-         * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
-         * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
-         */
+          try {
+              // write the file path
+              writer.write(new StringBuilder(file.getPath() + "\n").toString());
+          } catch (IOException e) {
+              LOG.log(Level.WARNING, "Could not write path. {0}", e.getMessage());
+
+          }
       }
     });
   }
--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -9,6 +9,9 @@ import java.util.logging.Logger;
 public class Utils {
 
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+  private static final String WIN_SEPARATOR = "\r\n",
+          MACOS9_SEPARATOR = "\r",
+          UNIX_SEPARATOR = "\n";
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
@@ -20,7 +23,29 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    String[] linesTab;
+
+    //verif if there is a separator
+    if (!lines.contains(WIN_SEPARATOR) && !lines.contains(MACOS9_SEPARATOR) && !lines.contains(UNIX_SEPARATOR)) {
+      return new String[]{"", lines};
+
+    } // verif if there is a  WINDOWS separator
+    else if (lines.contains(WIN_SEPARATOR))
+    {
+      linesTab = lines.split(WIN_SEPARATOR, 2);
+      linesTab[0] = linesTab[0] + WIN_SEPARATOR;
+
+    } // verif if there is a  MACOS9 separator
+    else if (lines.contains(MACOS9_SEPARATOR))
+    {
+      linesTab = lines.split(MACOS9_SEPARATOR, 2);
+      linesTab[0] = linesTab[0] + MACOS9_SEPARATOR;
+
+    } // verif if there is a UNIX line separator
+    else {
+      linesTab = lines.split(UNIX_SEPARATOR, 2);
+      linesTab[0] = linesTab[0] + UNIX_SEPARATOR;
+    }
+    return linesTab;  }
 
 }

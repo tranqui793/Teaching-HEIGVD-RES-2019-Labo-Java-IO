35baf8ea48bde5febb03091d4f94eb0d66976a7c
olliens <oussama.lagha@heig-vd.ch>
2019-03-12 10:15:11 +0100

    cleanning project

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -53,11 +53,6 @@ public abstract class FileTransformer implements IFileVisitor {
       Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), "UTF-8"); // the bug fix by teacher
       writer = decorateWithFilters(writer);
 
-      /*
-       * There is a missing piece here: you have an input reader and an ouput writer (notice how the 
-       * writer has been decorated by the concrete subclass!). You need to write a loop to read the
-       * characters and write them to the writer.
-       */
       StringBuilder str = new StringBuilder();
       int charToRead;
       //read from the file with the reader
